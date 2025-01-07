51b05405256684330a10aac1384a66e65d44b8af
Calvin <CalvinB552@gmail.com>
2025-01-07 16:17:40 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# codebusting_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/codebusting_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.codebusting_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
